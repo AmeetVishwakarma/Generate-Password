32b535bd98d930a331b94d105449b803ec37901d
AmeetVishwakarma <100289318+AmeetVishwakarma@users.noreply.github.com>
2023-09-15 13:20:57 +0530

    uploaded

---
+++ app.js
@@ -0,0 +1,23 @@
+const passwordBox = document.getElementById('password');
+const lenght = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "!()*+,-./:;<=>?@[\]^_`{|}~";
+
+const allChar = upperCase + lowerCase + numbers + symbols;
+
+function generatePassword(){
+
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += numbers[Math.floor(Math.random() * numbers.length)];
+    password += symbols[Math.floor(Math.random() * symbols.length)];
+
+    while(lenght > password.length){
+    password += allChar[Math.floor(Math.random() * allChar.length)];    
+    }
+    passwordBox.value = password;
+}
